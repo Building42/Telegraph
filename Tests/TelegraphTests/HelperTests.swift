2c4245115face364ab887d12633878c32769849d
Zyphrax <yvo@yvo.net>
2018-07-24 12:31:46 -0400

    Fix tests on 32bit build system

--- Tests/TelegraphTests/HelperTests.swift
+++ Tests/TelegraphTests/HelperTests.swift
@@ -45,13 +45,13 @@ class HelperTests: XCTestCase {
     XCTAssertEqual(UInt64.min.bytes, [0, 0, 0, 0, 0, 0, 0, 0], "UInt64.min to bytes is incorrect")
     XCTAssertEqual(UInt64.max.bytes, [255, 255, 255, 255, 255, 255, 255, 255], "UInt64.max to bytes is incorrect")
 
-    XCTAssertEqual(UInt64(24).bytes, [0, 0, 0, 0, 0, 0, 0, 24], "UInt64 '24' to bytes is incorrect")
-    XCTAssertEqual(UInt64(12345).bytes, [0, 0, 0, 0, 0, 0, 48, 57], "UInt64 '12345' to bytes is incorrect")
-    XCTAssertEqual(UInt64(534231).bytes, [0, 0, 0, 0, 0, 8, 38, 215], "UInt64 '534231' to bytes is incorrect")
-    XCTAssertEqual(UInt64(1212121212).bytes, [0, 0, 0, 0, 72, 63, 128, 124], "UInt64 '1212121212' to bytes is incorrect")
-    XCTAssertEqual(UInt64(204060802040).bytes, [0, 0, 0, 47, 130, 248, 187, 248], "UInt64 '204060802040' to bytes is incorrect")
-    XCTAssertEqual(UInt64(78767574737271).bytes, [0, 0, 71, 163, 129, 79, 225, 119], "UInt64 '78767574737271' to bytes is incorrect")
-    XCTAssertEqual(UInt64(9999999999999999).bytes, [0, 35, 134, 242, 111, 192, 255, 255], "UInt64 '9999999999999999' to bytes is incorrect")
-    XCTAssertEqual(UInt64(1000100010001000100).bytes, [13, 225, 17, 169, 11, 249, 102, 164], "UInt64 '1000100010001000100' to bytes is incorrect")
+    XCTAssertEqual((24 as UInt64).bytes, [0, 0, 0, 0, 0, 0, 0, 24], "UInt64 '24' to bytes is incorrect")
+    XCTAssertEqual((12345 as UInt64).bytes, [0, 0, 0, 0, 0, 0, 48, 57], "UInt64 '12345' to bytes is incorrect")
+    XCTAssertEqual((534231 as UInt64).bytes, [0, 0, 0, 0, 0, 8, 38, 215], "UInt64 '534231' to bytes is incorrect")
+    XCTAssertEqual((1212121212 as UInt64).bytes, [0, 0, 0, 0, 72, 63, 128, 124], "UInt64 '1212121212' to bytes is incorrect")
+    XCTAssertEqual((204060802040 as UInt64).bytes, [0, 0, 0, 47, 130, 248, 187, 248], "UInt64 '204060802040' to bytes is incorrect")
+    XCTAssertEqual((78767574737271 as UInt64).bytes, [0, 0, 71, 163, 129, 79, 225, 119], "UInt64 '78767574737271' to bytes is incorrect")
+    XCTAssertEqual((9999999999999999 as UInt64).bytes, [0, 35, 134, 242, 111, 192, 255, 255], "UInt64 '9999999999999999' to bytes is incorrect")
+    XCTAssertEqual((1000100010001000100 as UInt64).bytes, [13, 225, 17, 169, 11, 249, 102, 164], "UInt64 '1000100010001000100' to bytes is incorrect")
   }
 }
