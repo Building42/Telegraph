b22b3dedaf0d3cf439ea867e9eeff171fc2f335b
John Turnipseed <john.turnipseed@nanonation.net>
2021-03-26 17:47:05 -0500

    Updated iOS minimum build target to 9.0 to address Xcode 12.x compiler issues

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Telegraph",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v9),
         .tvOS(.v9),
         .macOS(.v10_10)
     ],
